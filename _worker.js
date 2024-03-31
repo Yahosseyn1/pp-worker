038a34aad3f7453809816a6c4cb2427646477b3b
Yahosseyn1 <165310085+Yahosseyn1@users.noreply.github.com>
2024-03-31 22:51:44 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,10 +2,10 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+let userID = '90784dde-b79d-42eb-9936-90b5f56a05bd';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
-let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+let proxyIP = '141.145.199.188';
 
 let dohURL = 'https://1.1.1.1/dns-query';
 // (dohURL) list :
